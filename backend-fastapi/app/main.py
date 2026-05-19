66b3df817563f901aee4e2b2ea70067dc9eb478b
Mbeleck <mbeleckmbeleck@gmail.com>
2026-05-19 00:25:57 -0600

    configured the compose.yaml file

--- backend-fastapi/app/main.py
+++ backend-fastapi/app/main.py
@@ -5,8 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "testing docker"}
-
+    return {"Hello": "still doing further tests with docker"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
