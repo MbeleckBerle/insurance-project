51711b65591e21f945414a26c3a4ec64c27669f9
Mbeleck <mbeleckmbeleck@gmail.com>
2026-05-20 22:21:50 -0600

    connected to supabase

--- backend-fastapi/app/main.py
+++ backend-fastapi/app/main.py
@@ -1,13 +1,77 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, select
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from dotenv import load_dotenv
+from typing import Annotated
+
+from .db import db_connect
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Setting up FastAPI backend with Docker"}
+load_dotenv()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+SessionDep = Annotated[Session, Depends(db_connect.get_session)]
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str
+
+
+"""
+Create database on startup
+"""
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+        db_connect.create_db_and_tables()
+        yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+@app.post("/heroes/")
+def create_hero(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heroes/")
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "Setting up FastAPI backend with Docker"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str | None = None):
+#     return {"item_id": item_id, "q": q}
 
